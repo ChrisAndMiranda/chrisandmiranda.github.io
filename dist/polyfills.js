088ad0efb1e1d3f8c04e4e05448b434cda605ad9
Zach Walker <walkez5@nationwide.com>
2019-11-25 17:31:42 -0500

    add improvements to master

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -6841,8 +6841,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandafinal\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandafinal\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandaFinal\chrisandmiranda.github.io\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandaFinal\chrisandmiranda.github.io\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- polyfills-es5.js
+++ polyfills-es5.js
@@ -9133,8 +9133,8 @@ for (var COLLECTION_NAME in DOMIterables) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandafinal\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
-module.exports = __webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandafinal\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
+__webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandaFinal\chrisandmiranda.github.io\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
+module.exports = __webpack_require__(/*! C:\Users\walkez5\ngfun\chrismirandaFinal\chrisandmiranda.github.io\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
 
 
 /***/ })
