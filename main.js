8827e604652e02b4963a16cafdd94851fadc8c1c
Zach Walker <walkez5@nationwide.com>
2020-03-25 18:01:28 -0400

    change routing to get rid of the /home path and just have the one URL, which i believe is the cause of a 404

--- main.js
+++ main.js
@@ -234,8 +234,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var routes = [
-    { path: 'home', component: _home_home_component__WEBPACK_IMPORTED_MODULE_3__["HomeComponent"] },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: '', component: _home_home_component__WEBPACK_IMPORTED_MODULE_3__["HomeComponent"] }
 ];
 var AppRoutingModule = /** @class */ (function () {
     function AppRoutingModule() {
